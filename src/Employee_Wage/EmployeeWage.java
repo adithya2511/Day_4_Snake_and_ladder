2cea83c4796201a8b075d1261291856827bbc50e
adithya89780 <adithyasachin123@gmail.com>
2022-08-13 14:45:54 +0530

    Added: Switch case statements

--- src/Employee_Wage/EmployeeWage.java
+++ src/Employee_Wage/EmployeeWage.java
@@ -8,21 +8,27 @@ public class EmployeeWage {
 
     static final int PART_TIME_HOUR =4;
 
+    static final int IS_PART_TIME = 1;
+    static final int IS_FULL_TIME = 2;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         int empPresent = (int)Math.floor(Math.random()*10) %3;
 
         int empWage = 0;
-        if(empPresent == 2) {
-            empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-            System.out.println("Employee FULL TIME");
-        }
-        else if (empPresent == 1) {
-            empWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-            System.out.println("Employee PART TIME");
-        } else
-            System.out.println("Employee Absent");
 
+        switch (empPresent){
+            case IS_FULL_TIME :
+                empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                System.out.println("Employee FULL TIME");
+                break;
+            case IS_PART_TIME:
+                empWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                System.out.println("Employee PART TIME");
+                break;
+            default:
+                System.out.println("Employee Absent");
+        }
         System.out.println("Employee Wage => "+empWage);
     }
 }
