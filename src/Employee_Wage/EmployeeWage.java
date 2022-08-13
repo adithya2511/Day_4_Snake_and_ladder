ccc0adfd8d732fb134706b1a16e41538ab9462c1
adithya89780 <adithyasachin123@gmail.com>
2022-08-13 14:02:59 +0530

    added: daily employee wage

--- src/Employee_Wage/EmployeeWage.java
+++ src/Employee_Wage/EmployeeWage.java
@@ -2,12 +2,19 @@ package Employee_Wage;
 
 public class EmployeeWage {
 
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOUR = 8;
+
     public static void main(String[] args) {
-        System.out.println(" Welcome to Employee Wage Computation ");
+        System.out.println("Welcome to Employee Wage Computation");
         int empAttendence = (int)Math.floor(Math.random()*10) %2;
-        if(empAttendence == 1)
+
+        int empWage = 0;
+        if(empAttendence == 1) {
+            empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
             System.out.println("Employee Present");
-        else
+        }else
             System.out.println("Employee Absent");
+        System.out.println("Employee Wage => "+empWage);
     }
 }
